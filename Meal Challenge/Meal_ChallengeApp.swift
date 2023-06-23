ce8a187a0e5c0b4d678ef020a357a43280ce3eca
MJ <57292219+mjrasoul1@users.noreply.github.com>
2023-06-23 12:25:09 -0400

    Initial Commit

---
+++ Meal Challenge/Meal_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_ChallengeApp.swift
+//  Meal Challenge
+//
+//  Created by Mj Rasoul on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Meal_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
